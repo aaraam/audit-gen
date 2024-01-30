fe81b87e0375dfdd4e3435f9c486334ba1c687d0
aaraam <aaramblkice@gmail.com>
2024-01-30 16:16:01 +0530

    feat: slither optimized

--- modules/slither.js
+++ modules/slither.js
@@ -1,17 +1,24 @@
-const { exec } = require("child_process");
-const path = require("path");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runSlither(solidityFilePath) {
+    try {
+        console.log(`Running Slither on ${solidityFilePath}`);
+
+        // Install and use specific Solidity compiler version
+        console.log('Installing Solidity compiler version 0.7.6...');
+        await exec('solc-select install 0.7.6');
+        console.log('Using Solidity compiler version 0.7.6...');
+        await exec('solc-select use 0.7.6');
+
+        // Run Slither
+        const { stdout } = await exec(`slither ${solidityFilePath}`);
+        console.log(`Slither Analysis Output:\n${stdout}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+    }
+}
 
 // Adjust this to the path of your Solidity file
-const solidityFilePath = path.join(__dirname, './samples/your_solidity_file.sol');
-
-exec(`slither ${solidityFilePath}`, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Stderr: ${stderr}`);
-        return;
-    }
-    console.log(`Slither Analysis Output:\n${stdout}`);
-});
+const solidityFilePath = 'samples/TimeLock.sol';
+runSlither(solidityFilePath);
